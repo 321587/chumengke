9ed8467a94eb8f734c20eb5720f0c7244e78ebd6
321587 <mac@MacdeiMac-15.local>
2016-03-12 14:52:29 +0800

    chu
    hello world

---
+++ GHelp/GHelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHelp
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
